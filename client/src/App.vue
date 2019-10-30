6f06836cce7fd46cf04f5e31b51ad1b0be3b79ca
Ross Lorimer <wross.lorimer@googlemail.com>
2019-10-30 11:48:50 +0000

    re-add client

---
+++ client/src/App.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+  <div id="app">
+    <bookings-form />
+    <bookings-grid :bookings="bookings"/>
+  </div>
+</template>
+
+<script>
+import BookingsForm from "./components/BookingsForm.vue";
+import BookingsGrid from "./components/BookingsGrid.vue";
+import { eventBus } from "./main";
+import BookingService from "./services/BookingService";
+
+export default {
+  name: "app",
+  data () {
+    return {
+      bookings: []
+    }
+  },
+  components: {
+    'bookings-form': BookingsForm,
+    'bookings-grid': BookingsGrid
+  },
+  mounted(){
+    this.fetchData();
+
+  },
+  methods: {
+    fetchData(){
+      BookingService.getBookings()
+      .then(bookings => this.bookings = bookings)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
